a6f5ca023c8015fa3009e2c8d032951b74d08437
xWildhagen <github@wildhagen.net>
2024-09-11 21:15:37 +0200

    Fix

--- configuration.nix
+++ configuration.nix
@@ -81,19 +81,6 @@ in
     xwayland.enable = true;
   };
 
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-  };
-
-  hardware = {
-    opengl.enable = true;
-    nvidia.modesetting.enable = true;
-  };
-
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
   #-------------------------------------------------------------------------------------------
   # USER
   #-------------------------------------------------------------------------------------------

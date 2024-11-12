39cfdd54d7368cc84fb2ee04e8603b9a04ebf62c
xWildhagen <github@wildhagen.net>
2024-11-12 17:59:45 +0100

    Formatting fixes

--- configuration.nix
+++ configuration.nix
@@ -6,8 +6,8 @@
 
 let
   version = "24.11";
-  locale = "nb_NO.UTF-8";
-  user = "wildhagen";
+  locale  = "nb_NO.UTF-8";
+  user    = "wildhagen";
 in
 {
   imports =
@@ -27,8 +27,8 @@ in
   system.autoUpgrade.enable = true;
 
   # Bootloader
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.enable      = true;
+  boot.loader.grub.device      = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
   # Enable QEMU Guest agent
@@ -36,39 +36,39 @@ in
 
   # Networking
   networking.networkmanager.enable = true;
-  #networking.wireless.enable = true;
-  networking.hostName = "nixos";
+  #networking.wireless.enable       = true;
+  networking.hostName              = "nixos";
   
   # Regional, language and time zone
-  time.timeZone = "Europe/Oslo";
+  time.timeZone      = "Europe/Oslo";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = locale;
+    LC_ADDRESS        = locale;
     LC_IDENTIFICATION = locale;
-    LC_MEASUREMENT = locale;
-    LC_MONETARY = locale;
-    LC_NAME = locale;
-    LC_NUMERIC = locale;
-    LC_PAPER = locale;
-    LC_TELEPHONE = locale;
-    LC_TIME = locale;
+    LC_MEASUREMENT    = locale;
+    LC_MONETARY       = locale;
+    LC_NAME           = locale;
+    LC_NUMERIC        = locale;
+    LC_PAPER          = locale;
+    LC_TELEPHONE      = locale;
+    LC_TIME           = locale;
   };
 
   # Keyboard
   services.xserver.xkb = {
-    layout = "no";
+    layout  = "no";
     variant = "winkeys";
   };
   console.keyMap = "no";
 
   # Sound
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  security.rtkit.enable      = true;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
+    enable            = true;
+    alsa.enable       = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
+    pulse.enable      = true;
   };
 
   # Printing
@@ -88,10 +88,10 @@ in
   #-------------------------------------------------------------------------------------------
   
   users.users.${user} = {
-    isNormalUser = true;
-    description = "Wildhagen";
-    home = "/home/wildhagen";
-    extraGroups = [ "networkmanager" "wheel" "video" "audio" "lp" "scanner" ];
+    isNormalUser    = true;
+    description     = "Wildhagen";
+    home            = "/home/wildhagen";
+    extraGroups     = [ "networkmanager" "wheel" "video" "audio" "lp" "scanner" ];
     initialPassword = user;
     packages = with pkgs; [
       kdePackages.kate

222707af9b25cf4776239a3647ab658a81291b76
Fredrik Wildhagen <github@wildhagen.net>
2025-07-02 00:52:03 +0200

    Fix

--- setup.sh
+++ setup.sh
@@ -10,4 +10,4 @@ sudo rm /etc/nixos/configuration.nix
 sudo ln -s ~/NixOS/configuration.nix /etc/nixos/configuration.nix
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-${VERSION}.tar.gz home-manager
 nix-channel --update
-nixos-rebuild switch
+sudo nixos-rebuild switch

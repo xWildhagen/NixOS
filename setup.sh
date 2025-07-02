17b6c8e7c948878a4b174e0e3dcdff6a1ec29a00
Fredrik Wildhagen <github@wildhagen.net>
2025-07-02 15:22:24 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -6,10 +6,12 @@ echo "Usage: sudo $0 VERSION"
 exit 1
 fi
 
-sudo rm /etc/nixos/configuration.nix
-sudo ln -s /home/nixos/nixos/configuration.nix /etc/nixos/configuration.nix
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-${VERSION}.tar.gz home-manager
-nix-channel --update
-sudo nixos-generate-config
-sudo chmod 755 /home/nixos/nixos/configuration.nix
-sudo nixos-install --root /etc/nixos/configuration.nix
+
+
+#sudo rm /etc/nixos/configuration.nix
+#sudo ln -s /home/nixos/nixos/configuration.nix /etc/nixos/configuration.nix
+#nix-channel --add https://github.com/nix-community/home-manager/archive/release-${VERSION}.tar.gz home-manager
+#nix-channel --update
+#sudo nixos-generate-config
+#sudo chmod 755 /home/nixos/nixos/configuration.nix && sudo chmod 755 /home/nixos
+#sudo nixos-install --root /home/nixos/nixos/configuration.nix
